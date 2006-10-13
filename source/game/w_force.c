3201a9ba4395cd78b84ec9e290bcdc11d7630fb1
Bradley Olson <razorace@hotmail.com>
2006-10-13 09:29:06 +0000

    DodgeSys:  Set the initial DP of players to 50 and rescaled the DP for Jedi skills to .5 and DP for merc skills to 1/6. ForceSys:  There's no longer a "crippled" period after being Force Gripped.

--- source/game/w_force.c
+++ source/game/w_force.c
@@ -152,14 +152,13 @@ const int mindTrickTime[NUM_FORCE_POWER_LEVELS] =
 
 
 //[DodgeSys]
-#define SK_DP_FORFORCE		1.0f	//determines the number of DP points players get for each skill point dedicated to Force Powers.
-#define SK_DP_FORMERC		1/3.0f	//determines the number of DP points get for each skill point dedicated to gunner/merc skills.	
-//[/DodgeSys]
+#define SK_DP_FORFORCE		.5f	//determines the number of DP points players get for each skill point dedicated to Force Powers.
+#define SK_DP_FORMERC		1/6.0f	//determines the number of DP points get for each skill point dedicated to gunner/merc skills.
 void DetermineDodgeMax(gentity_t *ent)
 {//sets the maximum number of dodge points this player should have.  This is based on their skill point allociation.
 	int i;
 	int skillCount;
-	float dodgeMax = 0;
+	float dodgeMax = 50;
 
 	//force powers
 	for(i = 0; i < NUM_FORCE_POWERS; i++)
@@ -4347,6 +4346,9 @@ void WP_ForcePowerStop( gentity_t *self, forcePowers_t forcePower )
 		{
 			
 			g_entities[self->client->ps.fd.forceGripEntityNum].client->ps.forceGripChangeMovetype = PM_NORMAL;
+			//[ForceSys]
+			g_entities[self->client->ps.fd.forceGripEntityNum].client->ps.fd.forceGripCripple = 0;
+			//[/ForceSys]
 		}
 
 		if (self->client->ps.forceHandExtend == HANDEXTEND_FORCE_HOLD)
