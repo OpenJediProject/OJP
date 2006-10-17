65d7107c427046e3be11161aeaea90afa5348ca0
Bradley Olson <razorace@hotmail.com>
2006-10-17 02:25:19 +0000

    SaberLockSys:  TABBots now try to use super breaks intelligently.

--- source/game/ai_tab.c
+++ source/game/ai_tab.c
@@ -4167,6 +4167,18 @@ void TAB_StandardBotAI(bot_state_t *bs, float thinktime)
 	}
 	//[/LedgeGrab]
 
+	//[SaberLockSys]
+	if(bs->cur_ps.saberLockTime > level.time)
+	{//bot is in a saber lock
+		//bots cheat by knowing their enemy's DP level, if they're low on DP, try to super break finish them.
+		if(g_entities[bs->cur_ps.saberLockEnemy].client->ps.stats[STAT_DODGE] < 50)
+		{
+			trap_EA_Attack(bs->client);
+		}
+		return;
+	}
+	//[/SaberLockSys]
+	
 	VectorCopy(bs->DestPosition, bs->lastDestPosition);
 
 
