c67c5d0dd241cde42552afd1373a632bf33b4ed5
Bradley Olson <razorace@hotmail.com>
2006-10-20 03:35:15 +0000

    WeaponSys: set alt-fire fire rate to be the same as the primary fire.

--- source/game/bg_weapons.c
+++ source/game/bg_weapons.c
@@ -305,9 +305,10 @@ weaponData_t weaponData[WP_NUM_WEAPONS] =
 		8192,					//	int		range;				// Range of weapon
 		//[WeaponSys]
 		1,						//	int		altEnergyPerShot;	// Amount of energy used for alt-fire
-		//2,						//	int		altEnergyPerShot;	// Amount of energy used for alt-fire
+		3600,					//	int		altFireTime;		// Amount of time between alt-firings
+		//2,					//	int		altEnergyPerShot;	// Amount of energy used for alt-fire
+		//1200,					//	int		altFireTime;		// Amount of time between alt-firings
 		//[/WeaponSys]
-		1200,					//	int		altFireTime;		// Amount of time between alt-firings
 		8192,					//	int		altRange;			// Range of alt-fire
 		0,						//	int		chargeSubTime;		// ms interval for subtracting ammo during charge
 		0,						//	int		altChargeSubTime;	// above for secondary
