bf03f1cf6e81e2febd97df50dd0ca614a19838a6
Briar Bowser <briarbear7@gmail.com>
2007-03-22 18:53:19 +0000

    g_minExpLevel changed from 16 to 20. g_maxExpLevel changed from 200 to 250. Updated docs.

--- source/game/g_main.c
+++ source/game/g_main.c
@@ -389,8 +389,8 @@ static cvarTable_t		gameCvarTable[] = {
 	
 	//[ExpSys]
 	//sets the initial skill points that players get.
-	{ &g_minForceRank, "g_minExpLevel", "16", CVAR_ARCHIVE, 0, qfalse  },
-	{ &g_maxForceRank, "g_maxExpLevel", "200", CVAR_ARCHIVE, 0, qfalse  },
+	{ &g_minForceRank, "g_minExpLevel", "20", CVAR_ARCHIVE, 0, qfalse  }, //Default was 16
+	{ &g_maxForceRank, "g_maxExpLevel", "250", CVAR_ARCHIVE, 0, qfalse  },//Default was 250
 	//{ &g_maxForceRank, "g_maxForceRank", "6", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_LATCH, 0, qfalse  },
 	//[/ExpSys]
 	{ &g_forceBasedTeams, "g_forceBasedTeams", "0", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_LATCH, 0, qfalse  },
