ff97b3a5213d3db8c93e420dafefc0c106514505
Bradley Olson <razorace@hotmail.com>
2007-06-09 09:36:09 +0000

    Autobuilder:  Fixed problem where *.nav were getting put into the Pk3s and taking up space and slowing down the build process.

--- MakePK3.bat
+++ MakePK3.bat
@@ -18,7 +18,7 @@ ECHO ===========
 ECHO Making Pk3s
 ECHO ===========
 
-..\Utilities\zip\7za.exe a -tzip %PK3ASSETS%.pk3 .\%ASSETSFOLDER%\* -xr!.svn\ -x!*.dll -x!*.so -x!.\%ASSETSFOLDER%\*.* -x!*.nav -mx9
+..\Utilities\zip\7za.exe a -tzip %PK3ASSETS%.pk3 .\%ASSETSFOLDER%\* -xr!.svn\ -x!*.dll -x!*.so -x!.\%ASSETSFOLDER%\*.* -xr!*.nav -mx9
 IF ERRORLEVEL 1 GOTO ERROR
 ..\Utilities\zip\7za.exe a -tzip %PK3DLL%.pk3 .\%ASSETSFOLDER%\*.dll -mx9
 
