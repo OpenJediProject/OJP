031f45cdc92a75fa305478a2d9f9b5a8d4652be7
Bradley Olson <razorace@hotmail.com>
2015-02-21 22:59:44 +0000

    FatigueSys: Removed 1 second delay to FP regeneration after performing an action that blocks FP regeneration.

--- source/game/bg_public.h
+++ source/game/bg_public.h
@@ -114,7 +114,7 @@
 //FP cost of saber jump attacks
 #define FATIGUE_JUMPATTACK		-FATIGUE_JUMP + 3
 
-#define FATIGUE_REGEN_DEBOUNCE	1000	//delay to fatigue regen after doing something you can't regen FP during.
+#define FATIGUE_REGEN_DEBOUNCE	g_forceRegenTime.integer	//delay to fatigue regen after doing something you can't regen FP during.
 #define FATIGUE_MEDITATE_DELAY	4000											//delay to accelerated fatigue regen for meditation.
 //[/FatigueDefines]
 //[/FatigueSys]
